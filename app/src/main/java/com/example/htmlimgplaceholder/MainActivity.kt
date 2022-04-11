cbba3fba45e11b1b0eba8cc5123b5e55e9622972
Cheticamp <shopn08@gmail.com>
2022-04-11 09:38:14 -0400

    Simplify placeholder logic.

--- app/src/main/java/com/example/htmlimgplaceholder/MainActivity.kt
+++ app/src/main/java/com/example/htmlimgplaceholder/MainActivity.kt
@@ -4,27 +4,27 @@ package com.example.htmlimgplaceholder
 
 import android.annotation.SuppressLint
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.ColorFilter
-import android.graphics.PixelFormat
-import android.graphics.Rect
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.Drawable
-import android.graphics.drawable.DrawableWrapper
 import android.os.Build
 import android.os.Bundle
-import android.text.*
-import android.text.style.MetricAffectingSpan
+import android.text.Html
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.ImageSpan
 import android.util.Log
 import android.widget.TextView
-import androidx.annotation.RequiresApi
+import androidx.annotation.DrawableRes
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
-import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(), CoroutineScope by MainScope() {
     private lateinit var tvBody: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,115 +32,98 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         tvBody = findViewById(R.id.tv_body)
         val exampleText =
-            "Example <br> <img src=\"https://www.w3schools.com/images/w3schools_green.jpg\" alt=\"W3Schools.com\"> <br> Example"
+            "W3 Schools Image<br><img src=\"https://www.w3schools.com/images/w3schools_green.jpg\"><br>Google Image<br><img src=\"https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png\">"
         tvBody.setText(fromHtml(exampleText, this), TextView.BufferType.SPANNABLE)
     }
 
+    private var delaySeconds = MAX_DELAY
+    private var downloadIndex = 0
+    private val downloadedImages = intArrayOf(R.drawable.downloaded_1, R.drawable.downloaded_2)
+
+    @Suppress("DEPRECATION", "SameParameterValue")
     private fun fromHtml(html: String?, context: Context): Spannable {
         // Define the ImageGetter for Html. The default "no image, yet" drawable is
         // R.drawable.placeholder but can be another drawable.
+        val placeHolderImage =
+            BitmapFactory.decodeResource(context.resources, R.drawable.placeholder)
+
         val imageGetter = Html.ImageGetter { url ->
-            val dr = ContextCompat.getDrawable(context, R.drawable.placeholder) as BitmapDrawable
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                ImageWrapperApi23(dr)
-            } else {
-                ImageWrapper(dr)
-            }.apply {
-                // Simulate a network fetch of the real image we want to display.
-                simulateNetworkFetch(context, this, url)
+            // Simulate a network fetch of the real image we want to display.
+            val placeHolder = PlaceHolder(context, placeHolderImage, 100, 100)
+            simulateNetworkFetch(
+                context,
+                placeHolder,
+                url,
+                delaySeconds,
+                downloadedImages[downloadIndex]
+            )
+            // Stagger multiple fetches.
+            delaySeconds -= 2
+            if (delaySeconds <= 0) {
+                delaySeconds = MAX_DELAY
             }
+            downloadIndex = (downloadIndex + 1) % 2
+            placeHolder
         }
         return SpannableString(
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
                 Html.fromHtml(html, Html.FROM_HTML_MODE_LEGACY, imageGetter, null)
             } else {
+                @Suppress("SameParameterValue")
                 Html.fromHtml(html, imageGetter, null)
             }
         )
     }
 
     @SuppressLint("NewApi")
-    private fun simulateNetworkFetch(context: Context, imageWrapper: Drawable, url: String) {
-        GlobalScope.launch {
+    private fun simulateNetworkFetch(
+        context: Context,
+        placeHolder: Drawable,
+        url: String,
+        seconds: Long,
+        @DrawableRes imageToShow: Int
+    ) {
+        launch {
             Log.d("Applog", "Simulating fetch of $url")
             // Just wait for a busy network to get back to us.
-            delay(4000)
+            delay(seconds * 1000)
             // Get the "downloaded" image and place it in our image wrapper.
-            val dr = ContextCompat.getDrawable(context, R.drawable.downloaded) as BitmapDrawable
-            if (imageWrapper is ImageWrapper) {
-                imageWrapper.setBitmapDrawable(dr)
-            } else {
-                (imageWrapper as ImageWrapperApi23).drawable = dr
-            }
-            // Force a remeasure/relayout of the TextView with the new image. invalidate() and
-            // requestLayout() should work (?) but don't.
+            val downloadedImage =
+                BitmapFactory.decodeResource(context.resources, imageToShow)
+            // Force a remeasure/relayout of the TextView with the new image.
             this@MainActivity.runOnUiThread {
-                // This is the quick way but may be expensive.
-//                tvBody.text = tvBody.text
-                // This is a clever way but really?
-                val text = tvBody.text as Spannable
-                text.setSpan(ForceLayoutSpan(), 0, text.length, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+                replaceSpan(context, tvBody.text as Spannable, placeHolder, downloadedImage)
             }
         }
     }
 
-    // Has to extend MetricAffectingSpan to force size change.
-    private class ForceLayoutSpan : MetricAffectingSpan() {
-        override fun updateDrawState(tp: TextPaint?) {
-        }
-
-        override fun updateMeasureState(textPaint: TextPaint) {
-        }
-
-    }
-
-    // Simple wrapper for a BitmapDrawable. Use this for API 22-.
-    private class ImageWrapper(d: BitmapDrawable) : Drawable() {
-        private lateinit var mBitMapDrawable: BitmapDrawable
-
-        init {
-            setBitmapDrawable(d)
-        }
-
-        override fun draw(canvas: Canvas) {
-            mBitMapDrawable.draw(canvas)
-        }
-
-        override fun setAlpha(alpha: Int) {
-        }
-
-        override fun setColorFilter(colorFilter: ColorFilter?) {
-        }
-
-        override fun getOpacity(): Int {
-            return PixelFormat.OPAQUE
-        }
-
-        fun setBitmapDrawable(bitmapDrawable: BitmapDrawable) {
-            mBitMapDrawable = bitmapDrawable
-            mBitMapDrawable.setBounds(
-                0,
-                0,
-                mBitMapDrawable.intrinsicWidth,
-                mBitMapDrawable.intrinsicHeight
-            )
-            bounds = mBitMapDrawable.bounds
+    // Replace the span that is holding the placeholder with the span of the fetched image.
+    private fun replaceSpan(
+        context: Context,
+        text: Spannable,
+        placeHolder: Drawable,
+        bitmap: Bitmap
+    ) {
+        val imageSpan = text.getSpans(0, text.length, ImageSpan::class.java)
+            .first { it.drawable == placeHolder } ?: return
+        text.apply {
+            val spanStart = getSpanStart(imageSpan)
+            val spanEnd = getSpanEnd(imageSpan)
+            val spanFlags = getSpanFlags(imageSpan)
+            removeSpan(imageSpan)
+            setSpan(ImageSpan(context, bitmap), spanStart, spanEnd, spanFlags)
         }
     }
 
-    // Simple wrapper for a BitmapDrawable for API 23+.
-    @RequiresApi(Build.VERSION_CODES.M)
-    private class ImageWrapperApi23(dr: BitmapDrawable) : DrawableWrapper(dr) {
+    companion object {
+        private const val MAX_DELAY = 4L
+    }
+
+    class PlaceHolder(context: Context, bitmap: Bitmap, width: Int, height: Int) :
+        BitmapDrawable(context.resources, bitmap) {
 
         init {
-            drawable = dr
-        }
-
-        override fun setDrawable(dr: Drawable?) {
-            dr?.also {
-                bounds = Rect(0, 0, it.intrinsicWidth, it.intrinsicHeight)
-            }
-            super.setDrawable(dr)
+            setBounds(0, 0, width, height)
         }
     }
 }
